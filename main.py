6c052430ce42239229449fc7228b889b8bb96d22
LordRonin <ronin.astaldo@gmail.com>
2022-05-17 15:03:47 +0600

    Configure PostgreSQL

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from datetime import date
 from functools import wraps
 
@@ -12,7 +14,7 @@ from forms import CreatePostForm, CreateRegistrationForm, CreateLoginForm, Creat
 from flask_gravatar import Gravatar
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(
@@ -20,7 +22,7 @@ gravatar = Gravatar(
 )
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///blog.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
